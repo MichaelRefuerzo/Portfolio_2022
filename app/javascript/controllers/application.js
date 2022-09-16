6c196b1ae4efd71d7f12455eeb5d9e007c74009c
Michael Refuerzo <chaelrefuerzo@gmail.com>
2022-09-17 05:08:21 +0800

    fixed frontend bugs and added animation effect in about page

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -7,3 +7,19 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
+
+// scroll fade up
+$(document).on("scroll", function() {
+  var pageTop = $(document).scrollTop();
+  var pageBottom = pageTop + $(window).height();
+  var tags = $(".tag");
+
+  for (var i = 0; i < tags.length; i++) {
+    var tag = tags[i];
+    if ($(tag).position().top < pageBottom) {
+      $(tag).addClass("visible");
+    } else {
+      $(tag).removeClass("visible");
+    }
+  }
+});
